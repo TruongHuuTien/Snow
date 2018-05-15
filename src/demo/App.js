c58d85eac86ab8f274b4aba407cfc8c3a0db352a
Tien <truong.htien@gmail.com>
2018-05-15 11:03:06 -0400

    Move into demo Folder

---
+++ src/demo/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.scss';
+import Button from '../components/button/button';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Button />
+      </div>
+    );
+  }
+}
+
+export default App;
