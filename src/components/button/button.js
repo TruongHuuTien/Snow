6e22c141baed5e3f09b646fd54239934203bab46
Tien <truong.htien@gmail.com>
2018-05-15 16:41:46 -0400

    Icon component + Font-awesome 4.7.0

--- src/components/button/button.js
+++ src/components/button/button.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import Icon from '../icon';
 import './button.scss';
 
 class Button extends Component {
   render() {
     return (
       <button>
+        <Icon name='user' />
         Snow Button
       </button>
     );
---
+++ src/components/icon.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import '../lib/font-awesome-4.7.0/scss/font-awesome.scss';
+
+class Icon extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: props.name
+    }
+  }
+
+  render() {
+    console.log(this.state);
+    let classNames = ['fa', 'icon'];
+    classNames.push('fa-' + this.state.name);
+    return (
+      <i className={ classNames.join(' ') }></i>
+    );
+  }
+}
+
+export default Icon;
