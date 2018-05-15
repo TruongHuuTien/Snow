159243e1dd03800ff62dc3fcd9f5f22874ebf3b0
Tien <truong.htien@gmail.com>
2018-05-15 11:03:23 -0400

    Button - Initial Commit

---
+++ src/components/button/button.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './button.scss';
+
+class Button extends Component {
+  render() {
+    return (
+      <button></button>
+    );
+  }
+}
+
+export default Button;
